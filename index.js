59000fce36afdb98e042371d5853c47f347d3094
cfclaudio <claudio.charmaine@gmail.com>
2020-06-19 15:09:16 -0400

    added ejs as the templating engine

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
+const { appendFileSync } = require('fs');
 
 const app = new express();
+
+// Tell Express to use EJS as our templating engine,
+// and that any file ending in .ejs should be rendered with the EJS package
+app.set('view engine', 'ejs');  
 
 // Register a public folder for static files
 app.use(express.static('public'));
@@ -11,17 +17,18 @@ app.listen(4000, ()=>{
 })
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    // res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+    res.render('index');
 })
 
 app.get('/about', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+    res.render('about');
 })
 
 app.get('/contact', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+    res.render('contact');
 })
 
 app.get('/post', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+    res.render('post');
 });
