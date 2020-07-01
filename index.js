9013f034df02085acc50d4110b1ca2775305dab4
cfclaudio <claudio.charmaine@gmail.com>
2020-07-01 18:08:20 -0400

    chapter-6: Saving posts to database, and dynamically listing onto homepage

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const path = require('path');
+
+const app = new express();
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const BlogPost = require('./models/BlogPost')
 
-mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: True});
+
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
 
 // Test connection
 const db = mongoose.connection;
@@ -12,11 +17,14 @@ db.once('open', function(){
     console.log('successfully connected to database');
 });
 
-const app = new express();
+// Body parser set up
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 // Tell Express to use EJS as our templating engine,
 // and that any file ending in .ejs should be rendered with the EJS package
-app.set('view engine', 'ejs');  
+app.set('view engine', 'ejs');
 
 // Register a public folder for static files
 app.use(express.static('public'));
@@ -25,9 +33,12 @@ app.listen(4000, ()=>{
     console.log('App is listening on port 4000...');
 })
 
-app.get('/', (req,res)=>{
-    // res.sendFile(path.resolve(__dirname, 'pages/index.html'));
-    res.render('index');
+
+// ---- Page Routing ----------------------------
+
+app.get('/', async (req,res)=>{
+    const blogposts = await BlogPost.find({});
+        res.render('index', {blogposts});
 })
 
 app.get('/about', (req,res)=>{
@@ -38,6 +49,26 @@ app.get('/contact', (req,res)=>{
     res.render('contact');
 })
 
-app.get('/post', (req,res)=>{
-    res.render('post');
-});
+app.get('/post/:id', async (req,res)=>{
+    const blogpost = await BlogPost.findById(req.params.id);
+    res.render('post', { blogpost });
+})
+
+app.get('/posts/new', (req,res)=>{
+    res.render('create');
+})
+
+app.get('/posts/search', (req,res)=>{
+    console.log('searching...');
+
+    // BlogPost.find({
+    //     title: req.body.search_input
+    // }, (error, blogspot)=>{
+    //     console.log(error, blogspot)
+    // })
+})
+
+app.post('/posts/store', async (req,res)=>{
+    await BlogPost.create(req.body);
+    res.redirect('/');
+})
--- models/BlogPost.js
+++ models/BlogPost.js
@@ -3,18 +3,24 @@ const Schema = mongoose.Schema;
 
 const BlogPostSchema = new Schema({
     title: String,
-    body: String
+    body: String,
+    username: String,
+    datePosted: {
+        type: Date,
+        default: new Date()
+    }
 });
 
 const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
 module.exports = BlogPost;
+
 
 // we export the BlogPost variable so that when other files require this file, they 
 // know to grab BlogPost. Note that you can only export one variable.
 
 
 
-// Models are defind through the Schema interfact 
+// Models are defind through the Schema interface 
 // Collection represents an entity in our app, eg user, products, blogposts
 // A schema represents how a collection looks like
 // Each document in the collection would have the fields specified in the schema
